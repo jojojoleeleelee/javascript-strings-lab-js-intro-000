d02db0957f7c3032030261ecc8f852cf4290bccd
Joanne Lee <vernapii@gmail.com>
2018-03-13 04:33:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, Everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Neil deGrasse Tyson" + specialGuest + "Hello, !";
+var greetSpecialGuest = "Neil deGrasse Tyson" + specialGuest + "Hello, ${specialGuest}!";
 
 var topic = "space";
 
